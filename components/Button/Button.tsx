e2b5e3e06223da5a21394bd6a5c4efe8725f66da
Gervis Bermudez <gervis.bermudez@tsoftlatam.com>
2023-01-19 16:55:35 -0300

    update button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Button({ text = "Button" }) {
+export default function Button(props: any) {
   return (
-    <button className="rounded py-1 px-2 border border-solid border-app-dp-600 text-app-dp-600">
-      {text}
+    <button
+      className="rounded bg-transparent py-1 px-2 border border-solid border-app-dp-600 text-app-dp-600 hover:bg-app-white focus:bg-app-surface"
+      {...props}
+    >
+      {props.children}
     </button>
   );
 }
